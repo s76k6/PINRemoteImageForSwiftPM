73cbb431c6e8f2f3f0dc1a4aadc7adbdd0f959be
Suiki Tanaka <suiki@tnaka.net>
2020-07-31 00:52:56 +0900

    add: Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+import PackageDescription
+
+let package = Package(
+    name: "PINRemoteImage",
+    products: [
+        .library(
+            name: "PINRemoteImage",
+            targets: ["PINRemoteImage"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "PINRemoteImage",
+            dependencies: []),
+    ]
+)
